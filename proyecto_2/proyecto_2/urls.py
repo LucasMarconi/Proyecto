2c586c880b4a2558f61c3745785ffb3fd7f35944
Lucas <lucas.marconi009@gmail.com>
2023-09-22 10:22:50 -0300

    Paso 1 del proyecto

--- proyecto_2/proyecto_2/urls.py
+++ proyecto_2/proyecto_2/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import saludo
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('saludo/', saludo),
+    path('muestra_nombre/<nombre>/', muestra_nombre),
+    path('probando_template/', probando_template),
 ]
