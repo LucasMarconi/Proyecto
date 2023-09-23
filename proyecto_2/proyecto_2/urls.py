468a29718914f046c4dd11fbc770a95466d09c2d
Lucas <lucas.marconi009@gmail.com>
2023-09-23 19:00:42 -0300

    Borrando las vistas

--- proyecto_2/proyecto_2/urls.py
+++ proyecto_2/proyecto_2/urls.py
@@ -16,13 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('saludo/', saludo),
-    path('muestra_nombre/<nombre>/', muestra_nombre),
-    path('probando_template/', probando_template),
-    path('usando_loader/', usando_loader),
-    path('curso/<nombre>/<numero>/', curso),
 ]
