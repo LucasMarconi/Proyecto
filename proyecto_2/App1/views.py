7c8bbbf6aa4212039308f530b4afa09ec183ffa8
Lucas <lucas.marconi009@gmail.com>
2023-09-25 15:46:30 -0300

    Modelo Pagina Web

--- proyecto_2/App1/views.py
+++ proyecto_2/App1/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def inicio(request):
+    return render(request, "App1/inicio.html")
+
+def cursos(request):
+    return HttpResponse("Vista cursos")
+
+def profesores(request):
+    return HttpResponse("Vista profesores")
+
+def estudiantes(request):
+    return HttpResponse("Vista estudiantes")
+
+def entregables(request):
+    return HttpResponse("Vista entregables")
