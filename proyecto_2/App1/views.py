923ef84a1e9be7f47395e5ed610bc03b350d00e5
Lucas <lucas.marconi009@gmail.com>
2023-09-29 23:22:26 -0300

    Agregamos Herencia e iniciamos el Admin de Django

--- proyecto_2/App1/views.py
+++ proyecto_2/App1/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def inicio(request):
-    return render(request, "App1/inicio.html")
+    return render(request, "App1/base.html")
 
 def cursos(request):
-    return HttpResponse("Vista cursos")
+    return render(request, "App1/cursos.html")
 
 def profesores(request):
     return HttpResponse("Vista profesores")
